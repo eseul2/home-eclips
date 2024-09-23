72dbbf465a6525e1fe4be1b8299eaeaf384aae37
esul2 <dltmfsus@naver.com>
2024-09-24 01:05:11 +0900

    ch02

---
+++ hello/src/Hello.java
@@ -0,0 +1,44 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+		
+	
+		
+		System.out.println("hello,world");
+		System.out.println("hello,world");
+	
+		System.out.println("hello,world");
+		System.out.println("hello,world");
+		System.out.println("hello,world");
+		System.out.println("hello,world");
+
+		int age = 20;
+		age = 20 + 12;
+		System.out.println(age);
+		
+
+		int x = 5;
+		int y = 3;
+		
+		System.out.println(x+y);
+		
+		final int score = 100;
+	//	score = 200;
+		System.out.println(score);
+		
+		
+		boolean power = false;
+		
+		float f = 3.14f;
+		
+		char ch = 'v';
+		
+		System.out.println(f);
+		
+		System.out.println(10.0/3);
+		
+		
+	}
+}
